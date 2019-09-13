bc715f9b96af6c4c50027e4cdfd1b2d16a5de74b
GrassMonk <GrassMonk@users.noreply.github.com>
2019-09-13 20:41:08 +1000

    Imported Assets from Neets zip

---
+++ CometProject/Assets/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    bool power = false;
+	void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().maxSpeed = 10f;
+            collision.gameObject.GetComponent<PlayerController>().acc = 1f;
+            Destroy(this.gameObject);
+        }
+    }
+}
