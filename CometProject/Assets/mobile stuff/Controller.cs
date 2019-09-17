2185da9bd781ca49216df66eba8e19ce746378d2
Asharrow777 <54733791+Asharrow777@users.noreply.github.com>
2019-09-17 19:53:19 +1000

    Mobilestuff added
    everything for mobile stuff added to folder called mobile stuff. this includes the scene, New Controller for player, controller for camera and the standard assets package from the unity store

---
+++ CometProject/Assets/mobile stuff/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Controller : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
