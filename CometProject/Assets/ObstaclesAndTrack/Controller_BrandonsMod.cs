1d4492de083756b00ac929be7434ca9e6ecec73d
Brandon Scolnik <brandon.scolnik@gmail.com>
2019-09-28 13:19:20 +1000

    Track prefabs & basic ring track
    Created track_straight and track_90deg_turn prefabs for reuse

---
+++ CometProject/Assets/ObstaclesAndTrack/Controller_BrandonsMod.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Controller_BrandonsMod : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    float dirX;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float moveVertical = CrossPlatformInputManager.GetAxis("Vertical");
+        //float moveHorizontal = Input.acceleration.x;
+        //float moveVertical = Input.acceleration.y;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
