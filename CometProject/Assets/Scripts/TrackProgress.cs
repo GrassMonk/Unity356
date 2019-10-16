cbd746ffb83b7d96e88e48ab6104a3c612915930
GrassMonk <GrassMonk@users.noreply.github.com>
2019-10-16 11:48:29 +1100

    Finish screen, Lap counting done

--- CometProject/Assets/Scripts/TrackProgress.cs
+++ CometProject/Assets/Scripts/TrackProgress.cs
@@ -9,18 +9,38 @@ public class TrackProgress : MonoBehaviour {
     private float totDis;
     private float crtDis;
     private float playerSize;
+    private TMPro.TextMeshProUGUI lapText;
+    private int lapInt;
+    public GameObject FinishMenu;
 
     // Use this for initialization
     void Start () {
         progress = 0;
+        lapInt = 1;
         playerSize = (GameObject.Find("Player").transform.localScale.y / 2) - 0.1f;
         totDis = Vector3.Distance(GameObject.Find("Player").transform.position, GameObject.Find("PlayerMarker").transform.position) - (20 + playerSize);
+        lapText = GameObject.Find("LapCounter").GetComponent<TMPro.TextMeshProUGUI>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         StartCoroutine("GetCrtDis");
+    }
+
+    public void IncrementLap()
+    {
+        lapInt++;
+        if (lapInt != 4)
+            lapText.text = (lapInt + "/3");
+        else
+        {
+            string place = GameObject.Find("Place").GetComponent<TMPro.TextMeshProUGUI>().text;
+            GameObject.Find("In-Game UI").SetActive(false);
+            Time.timeScale = 0;
+            FinishMenu.SetActive(true);
+            GameObject.Find("Place").GetComponent<TMPro.TextMeshProUGUI>().text = (""+place);
+        }
     }
 
     public void TotalDistance()
