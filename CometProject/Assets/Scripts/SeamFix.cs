4938fac0ac1e40d7f49a851f163db0613fb893d8
GrassMonk <GrassMonk@users.noreply.github.com>
2019-10-20 13:23:37 +1100

    Baked lighting added, Stopped sound from triggering over seams.

--- CometProject/Assets/Scripts/SeamFix.cs
+++ CometProject/Assets/Scripts/SeamFix.cs
@@ -7,10 +7,12 @@ public class SeamFix : MonoBehaviour {
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        GameObject.Find("BumpSound").GetComponent<AudioSource>().mute = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GameObject.Find("BumpSound").GetComponent<AudioSource>().mute = false;
     }
 }
